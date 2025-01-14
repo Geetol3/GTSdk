33696f8081043aab6184dd06a9c4e9dd388da5b0
阿景 <2869970810@qq.com>
2025-01-14 15:06:13 +0800

    nads新增channel字段

--- geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/beans/Nads.java
+++ geetolsdk/src/main/java/com/gtdev5/geetolsdk/mylibrary/beans/Nads.java
@@ -1,15 +1,16 @@
 package com.gtdev5.geetolsdk.mylibrary.beans;
 
 public class Nads {
-    private String code;
-    private String name;
-    private String number;
-    private String desc;
-    private int type;
+    private String code;//广告位置代码
+    private String name;//广告名称
+    private String number;//广告编号代码
+    private String desc;//广告说明
+    private int type;//（自营广告此字段无意义）广告类型：1：开屏，2：横幅，3：插屏，4：激励视频，5：全屏视频广告，6：draw信息流，7：新插屏广告等
+    private String channel;//广告渠道，例如[通用][oppo][vivo][华为][小米][荣耀]
     private int ips;
-    private String urlPoint;
-    private String urlResources;
-    private String remark;
+    private String urlPoint;//（非自营广告此字段无意义）自营广告跳转链接地址
+    private String urlResources;//（非自营广告此字段无意义）自营广告显示的资源连接
+    private String remark;//备注
     public void setCode(String code) {
         this.code = code;
     }
@@ -43,6 +44,13 @@ public class Nads {
     }
     public int getType() {
         return type;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+    public void setChannel(String channel) {
+        this.channel = channel;
     }
 
     public void setIps(int ips) {
